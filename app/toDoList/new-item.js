53e65aaa0e4c9389f8b284e2680f4f3d275518a4
Evan E <49208075+Evan-R-E@users.noreply.github.com>
2023-11-24 15:53:07 -0700

    To do list page

---
+++ app/toDoList/new-item.js
@@ -0,0 +1,71 @@
+
+import react, { useState } from "react";
+
+const NewItem = ({onAddItem}) => {
+    const [name, setName] = useState("");
+    const [priority, setPriority] = useState("Low priority");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        const item = {
+          name,
+          priority,
+        };
+        console.log(item);
+        
+        onAddItem(item);
+    
+        setName("");
+
+        setPriority("Low priority");
+
+    };
+  const handleDelete= ( deleteName) => {
+    const newName = name.filter((name) => name !== deleteName);
+    setPriority(newName)
+    
+  }  
+
+    return (
+        <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+          <div className="mb-4 ">
+            <label htmlFor="name" className="block font-bold">Need To Do:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              className="border border-gray-300 p-2 w-full text-black"
+            />
+          </div>
+  
+          <div className="mb-4">
+            <label htmlFor="priority" className="block font-bold t">Priority:</label>
+            <select
+              id="priority"
+              name="priority"
+              value={priority}
+              onChange={(e) => setPriority(e.target.value)}
+              className="border border-gray-300 p-2 w-full text-black"
+            >
+              <option value="Low">Low Priority</option>
+              <option value="Medium">Medium Priority</option>
+              <option value="High">High Priority</option>
+            </select>
+          </div>
+    
+          <button type="submit" className="w-1/2 bg-green-500 text-white px-4 py-2 rounded hover:bg-purple-700">
+            Add
+          </button>
+          <button onClick={() => handleDelete(name)} className="w-1/2 bg-red-500 text-white px-4 py-2 rounded hover:bg-purple-700">
+            Remove
+          </button>
+        </form>
+      );   
+};
+
+export default NewItem;
+
---
+++ app/toDoList/page.js
@@ -0,0 +1,23 @@
+
+"use client";
+import React, { useState } from "react";
+import ItemList from "./item-list";
+import NewItem from "./new-item.js";
+import itemsData from "./items.json";
+
+export default function Page(){
+    const [items, setItems] = useState(itemsData);
+    const handleAddItem = (newItem) => {
+        setItems([...itemsData, newItem]);
+    }
+    return(
+        
+        <main>
+            <h1 className="text-xl font-bold text-purple-300 text-center"> To-Do List</h1>
+            <NewItem onAddItem = {handleAddItem} /> {
+            }
+            <ItemList items = {items} />{
+            }
+        </main>
+    );
+}
